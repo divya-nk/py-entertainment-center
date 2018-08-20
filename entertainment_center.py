f0a5ec12e3157eeff69763f5fdfbb89266c82bf2
divya-nk <mnavane@g.clemson.edu>
2018-08-20 14:06:40 -0400

    inital commit

---
+++ entertainment_center.py
@@ -0,0 +1,11 @@
+import media
+
+toy_story = media.Movie("Toy Story", "A story of a boy and his toy blah.. blah", "https://en.wikipedia.org/wiki/File:Toy_Story.jpg", "https://www.youtube.com/watch?v=KYz2wyBy3kc")
+
+#print(toy_story.storyline)
+
+avatar = media.Movie("Avatar",
+	"A marine on an alien planet", "https://en.wikipedia.org/wiki/File:Avatar-Teaser-Poster.jpg",
+	"https://www.youtube.com/watch?v=6ziBFh3V1aM")
+
+print(avatar.storyline)
