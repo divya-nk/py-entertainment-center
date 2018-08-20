0f2e0cd62f468a11eef091e71faef37d2072228b
divya-nk <mnavane@g.clemson.edu>
2018-08-20 16:07:44 -0400

    updated with call to website

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,11 +1,22 @@
 import media
+import fresh_tomatoes
 
-toy_story = media.Movie("Toy Story", "A story of a boy and his toy blah.. blah", "https://en.wikipedia.org/wiki/File:Toy_Story.jpg", "https://www.youtube.com/watch?v=KYz2wyBy3kc")
+toy_story = media.Movie("Toy Story", "A story of a boy and his toy blah.. blah", 
+	"https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg", "https://www.youtube.com/watch?v=KYz2wyBy3kc")
 
 #print(toy_story.storyline)
 
 avatar = media.Movie("Avatar",
-	"A marine on an alien planet", "https://en.wikipedia.org/wiki/File:Avatar-Teaser-Poster.jpg",
+	"A marine on an alien planet", "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
 	"https://www.youtube.com/watch?v=6ziBFh3V1aM")
 
-print(avatar.storyline)
+#print(avatar.storyline)
+#avatar.show_trailer()
+
+raazi = media.Movie("Raazi", "A thriller story of an Indian Spy",
+	"https://upload.wikimedia.org/wikipedia/en/2/2f/Raazi_-_Poster.jpg",
+	"https://www.youtube.com/watch?v=YjMSttRJrhA")
+
+movies = [avatar, toy_story, raazi]
+
+fresh_tomatoes.open_movies_page(movies)
